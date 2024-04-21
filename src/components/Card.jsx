2cca168319016e0979c1a60e22238f05cdd42f25
Akshay Bhagwatkar <akshaybhagwatkar14@gmail.com>
2024-04-21 07:51:06 +0530

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Card.css";
+
+export default function Card({ card, handleChoice, flipped, cardDisabled }) {
+	// destructuring props
+	const handleClick = () => {
+		if (!cardDisabled) {
+			handleChoice(card);
+		}
+	};
+
+	return (
+		<div className="card">
+			<div className={flipped ? "hide flex" : "flex"}>
+				<div className="card-back" onClick={handleClick}></div>
+				<div className="card-front">{card.name}</div>
+			</div>
+		</div>
+	);
+}
